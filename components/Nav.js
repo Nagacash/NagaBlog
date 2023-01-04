d07c4475291d9f3ccb63927f562e6d6393bfe09d
Nagacash <chosenfewrecords@hotmail.de>
2023-01-04 18:35:50 +0000

    first commit

---
+++ components/Nav.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function Nav() {
+  return (
+    <nav>
+      {/* <Link href="/">Our Digital Showroom</Link> */}
+   
+      <img src="../NAGA_round.ico/" />
+      
+    </nav>
+  );
+}
+
+export default Nav;

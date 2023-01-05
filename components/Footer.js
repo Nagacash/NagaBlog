79611bb3ae5b14f34ac98e5d8e8ff57d645b80ea
Nagacash <chosenfewrecords@hotmail.de>
2023-01-05 16:50:00 +0000

    placed button in the center

--- components/Footer.js
+++ components/Footer.js
@@ -5,19 +5,20 @@ import 'bootstrap/dist/css/bootstrap.css'
 const Footer = () => {
     return (
         <footer id="sticky-footer footer container-fluid" className="flex-shrink-0 py-4 bg-dark text-white-50 width-auto">
-            <div className="container ">
-                <small className="text-white font-weight-light">Copyright &copy;
-                    <button className="rounded btn btn-warning m-3">
+            <div className="container text-center">
+                <button className="rounded btn btn-warning m-3">
 
-                        <a href="https://naga-clothing.com" className="text-black text-decoration-none font-weight-light">Naga Apparel</a>
+                    <a href="https://naga-clothing.com" className="text-black text-decoration-none font-weight-light">Naga Apparel</a>
 
-                        <small>
-                            <div className="text-white text-right m-1">
-                                Designed by: Maurice Holda
-                            </div>
-                        </small>
-                    </button>
-                </small>
+                    <small>
+                        <div className="text-white text-right m-1">
+                            Designed by: Maurice Holda
+                        </div>
+                    </small>
+                </button>
+                {/* <small className="text-white font-weight-light">Copyright &copy;
+
+                </small> */}
 
 
             </div>

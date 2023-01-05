8e28bee76565a4cb2d4c1295284f92826d04536e
Nagacash <chosenfewrecords@hotmail.de>
2023-01-05 17:46:04 +0000

    done

--- pages/_app.js
+++ pages/_app.js
@@ -8,16 +8,25 @@ function MyApp({ Component, pageProps }) {
     <>
       <div className="m-4 text-center">
         <Nav />
-        <h1 className="p-5 text1">Let's talk fashion, knowledge, and music! <br></br><br></br><h2>Our blog is your go-to for the latest style tips, the smartest insights, and the hottest tracks. Get ready to stay ahead of the game with us.</h2></h1>
+        <h1 className="p-5 text1">Let's talk fashion, knowledge, and music! </h1><br></br><br></br><h2>Our blog is your go-to for the latest style tips, the smartest insights, and the hottest tracks. Get ready to stay ahead of the game with us.</h2>
         <div className="">
-        <Component {...pageProps} />;
+          <Component {...pageProps} />;
         </div>
       </div>
 
-      <br></br><h1 className="p-5 text1   text-center">"Unlock the Mystical Sounds of the Naga: A Shortlord Productions - NAGA Sampler Vol. 1 <br></br> 
-      <div className="text-center">  Stream It Now on Spotify"!</div>
-    </h1><br></br><br></br>
-      <Hero /><br></br><br></br><br></br>
+      <br></br>
+      <h1 className="p-5 text1   text-center">"Unlock the Mystical Sounds of the Naga:<br></br> A Shortlord Productions.  
+      <br></br> <br></br>
+      <div className="text-warning text-small">- NAGA Sampler Vol. 1" -</div>
+      <br></br>
+        <div className="text-center">
+          Stream It Now on Spotify"!
+        </div>
+      </h1>
+
+      <br></br><br></br>
+      <Hero />
+      <br></br><br></br><br></br>
       <Footer />
 
     </>

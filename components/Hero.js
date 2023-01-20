dc6bc1a56f9a6c1e53ce0d9d802fdfea232ab28f
Nagacash <chosenfewrecords@hotmail.de>
2023-01-20 01:29:53 +0000

    blo post

--- components/Hero.js
+++ components/Hero.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 const Hero = () => {
     return (
+        
         <div>
 
             <iframe className="border-radius:12px " src="https://open.spotify.com/embed/album/7Ddzi7EjGjKnzHKuoqRKUQ?utm_source=generator" width="100%" height="380"  allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
